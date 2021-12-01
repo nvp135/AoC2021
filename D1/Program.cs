3cdba165ee2138fb44391960c244478eb044a902
Nikolay Platonov <juninhonvp@gmail.com>
2021-12-01 20:55:04 +0300

    D1 complete

--- D1/Program.cs
+++ D1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace D01
 {
@@ -6,7 +8,33 @@ namespace D01
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] measurements = System.IO.File.ReadAllText("input.txt").Split(Environment.NewLine).Select(Int32.Parse).ToArray();
+            Console.WriteLine(NumberOfIncreases(measurements));
+            Console.WriteLine(NumberOfThreeIncreases(measurements));
+        }
+
+        static int NumberOfIncreases(int[] measurements)
+        {
+            int count = 0;
+            for (int i = 1; i < measurements.Length; i++)
+            {
+                if (measurements[i] > measurements[i - 1])
+                    count++;
+            }
+            return count;
+        }
+
+        static int NumberOfThreeIncreases(int[] measurements)
+        {
+            int count = 0;
+            //int a = measurements[0], b = measurements[1], c = measurements[2];
+            for (int i = 3; i < measurements.Length; i++)
+            {
+                //int d = measurements[i];
+                if (measurements[i-3] + measurements[i-2] + measurements[i-1] < measurements[i - 2] + measurements[i - 1] + measurements[i])
+                    count++;
+            }
+            return count;
         }
     }
 }
